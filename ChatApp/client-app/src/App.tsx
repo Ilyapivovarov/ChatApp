da5411ff46a73f4b274fb84038a3ee8ba71ff36a
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-10-29 00:03:10 +0300

    Refactoring

--- ChatApp/client-app/src/App.tsx
+++ ChatApp/client-app/src/App.tsx
@@ -4,8 +4,7 @@ import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
 import ChatRoomPage from "./pages/ChatRoomPage/ChatRoomPage";
 import SignInPage from "./pages/SingInPage/SignInPage";
-import {Routs} from "./router/types/Routs";
-
+import {RouteTemplates} from "./router/types/Routs";
 
 import './App.css';
 
@@ -13,9 +12,9 @@ const App: React.FC = () => {
     return (
         <Layout>
             <Switch>
-                <Route exact path={Routs.Home} component={HomePage}/>
-                <Route path={Routs.ChatRoom} component={ChatRoomPage}/>
-                <Route path={Routs.SingIn} component={SignInPage} />
+                <Route exact path={RouteTemplates.Home} component={HomePage}/>
+                <Route path={RouteTemplates.ChatRoom} component={ChatRoomPage}/>
+                <Route path={RouteTemplates.SingIn} component={SignInPage}/>
             </Switch>
         </Layout>
     )
--- ChatApp/client-app/src/router/types/Routs.tsx
+++ ChatApp/client-app/src/router/types/Routs.tsx
@@ -1,4 +1,4 @@
-export enum Routs{
+export enum RouteTemplates {
     Home = "/",
     SingIn = "/sign-in",
     ChatRoom = "/chat-room/:id"
