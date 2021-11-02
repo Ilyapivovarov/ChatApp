5bbfeceee2b604566542c239d083a4fabeb7794c
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-02 21:22:38 +0300

    Add docker-compose.yml

--- ChatApp/Services/ServicesLocator.cs
+++ ChatApp/Services/ServicesLocator.cs
@@ -5,7 +5,7 @@ namespace ChatApp.Services
         public static string GetConnectionString(bool isDev)
         {
             const string devConStr = "Host=localhost;Port=5432;Database=chat-app-dev;Username=postgres;Password=sa";
-            const string prodConStr = "Host=172.18.0.2;Port=5432;Database=chat-app-prod;Username=postgres;Password=sa";
+            const string prodConStr = "Host=10.10.0.20;Port=5432;Database=chat-app-prod;Username=postgres;Password=sa";
 
             return isDev ? devConStr : prodConStr;
         }
