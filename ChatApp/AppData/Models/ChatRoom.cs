0d3d39028bdd9c58fc966e3d8482c41f825f3f67
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-02 21:51:42 +0300

    Update ChatRoom.cs

--- ChatApp/AppData/Models/ChatRoom.cs
+++ ChatApp/AppData/Models/ChatRoom.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using ChatApp.AppData.Models.Base;
 
 namespace ChatApp.AppData.Models
 {
     public class ChatRoom : DbModelBase
     {
+        [Required]
+        public List<User> Users { get; set; } = new List<User>();
         
+        [Required]
+        public List<ChatMessage> ChatMessages { get; set; } = new List<ChatMessage>();
     }
 }
