82a9d89b141d56aacad4949b42dde07d6a787112
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-02 15:26:30 +0300

    Add styles

--- ChatApp/client-app/src/components/Layout/Layout.tsx
+++ ChatApp/client-app/src/components/Layout/Layout.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { Container } from 'reactstrap';
 import NavMenu from '../NavMenu/NavMenu';
 
+import "./Layout.css"
+
 const Layout : React.FC = (props) => {
   return (
       <>
           <NavMenu />
-          <Container>
+          <Container className={"content-container"}>
               {props.children}
           </Container>
       </>
--- ChatApp/client-app/src/pages/SingInPage/SignInPage.tsx
+++ ChatApp/client-app/src/pages/SingInPage/SignInPage.tsx
@@ -1,40 +1,46 @@
 import React, {useState} from 'react';
-import {Button, Input} from "reactstrap";
+import {Button, Form, Input} from "reactstrap";
 import {useActions} from "../../hooks/useActions";
 import {useUserSelector} from "../../hooks/useAuth";
 import {Redirect} from "react-router-dom";
+
+import "./SignInPage.css"
 
 const SingInPage: React.FC = () => {
     const [username, setUsername] = useState("");
     const {signInUser} = useActions()
     const {error, isAuthorized} = useUserSelector(state => state.users);
-    
+
     if (isAuthorized)
         return (
             <Redirect to={"/"}/>
         )
-        
+
     return (
-        <div>
+        <>
             <h1>Sign in page</h1>
-            <div>
-                <Input
-                    value={username}
-                    onChange={(e) => {
-                        e.persist();
-                        setUsername((username) => e.target.value)
-                    }}
-                />
-                
-                <div>
-                    <h2>{error}</h2>
-                </div>
-                
-               <div>
-                   <Button onClick={() => signInUser(username)}> Sign in </Button>
-               </div>
+            <div className={"sign-in-wrapper"}>
+                <h2>Enter username</h2>
+                <Form className={"sign-in-form"}>
+                    <Input
+                        className={"name-input"}
+                        value={username}
+                        onChange={(e) => {
+                            e.persist();
+                            setUsername((username) => e.target.value)
+                        }}
+                    />
+
+                    <div>
+                        <h2>{error}</h2>
+                    </div>
+
+                    <div>
+                        <Button onClick={() => signInUser(username)}> Sign in </Button>
+                    </div>
+                </Form>
             </div>
-        </div>
+        </>
     );
 };
 
