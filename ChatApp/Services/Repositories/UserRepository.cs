35e7b990cdd41a6b485837f0aa36f44d8ac68d79
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-04 11:09:00 +0300

    Refactoring

--- ChatApp/Services/Repositories/UserRepository.cs
+++ ChatApp/Services/Repositories/UserRepository.cs
@@ -11,13 +11,13 @@ namespace ChatApp.Services.Repositories
 {
     public sealed class UserRepository : RepositoryBase, IUserRepository
     {
-        public async Task<bool> TrySignOnUserAsync(SignOn signOn)
+        public async Task<bool> TrySignUpUserAsync(SignUp signOn)
         {
             return await Task.Run(() =>
             {
                 return WriteData(db =>
                 {
-                    Services.Logger.LogTrace($"{nameof(TrySignOnUserAsync)} not implement");
+                    Services.Logger.LogTrace($"{nameof(TrySignUpUserAsync)} not implement");
                 }, "Error while writing user in database");
             });
         }
