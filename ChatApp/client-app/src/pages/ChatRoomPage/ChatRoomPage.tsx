0324a085d69d75ea6c3914d2a78fc626a254de3c
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-07 16:39:32 +0300

    Refactoring

--- ChatApp/client-app/src/pages/ChatRoomPage/ChatRoomPage.tsx
+++ ChatApp/client-app/src/pages/ChatRoomPage/ChatRoomPage.tsx
@@ -1,16 +1,15 @@
 import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
-import {useActions} from "../../hooks/useActions";
 import {useUserSelector} from "../../hooks/useAuth";
 import {useRoomActionCreator} from "../../hooks/useRoom";
 
 const ChatRoomPage: React.FC = () => {
-    const { id } = useParams<{id?: string}>();
-    const {room, error, loading} = useUserSelector(x => x.rooms);
+    const {room} = useUserSelector(x => x.rooms);
     const {fetchRoom} = useRoomActionCreator();
+    
     useEffect(() => {
         fetchRoom(1);
     },[] )
+    
     return (
         <div>
             <h1>Chat room page {room}</h1>
