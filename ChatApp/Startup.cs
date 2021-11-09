6092fae9ccb8603ebeafbe5b83bdd767c5283ce5
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-09 09:45:17 +0300

    Update target framework

--- ChatApp/Startup.cs
+++ ChatApp/Startup.cs
@@ -83,7 +83,7 @@ namespace ChatApp
 
                 if (_env.IsDevelopment())
                 {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    spa.UseReactDevelopmentServer("start");
                 }
             });
 
