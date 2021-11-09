fc460b64d0a327d31d8bcd9973434847d33bb5a5
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2021-11-09 15:07:05 +0300

    Add signalr

--- ChatApp/client-app/src/pages/ChatRoomPage/ChatRoomPage.tsx
+++ ChatApp/client-app/src/pages/ChatRoomPage/ChatRoomPage.tsx
@@ -1,11 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useUserSelector} from "../../hooks/useAuth";
 import {useRoomActionCreator} from "../../hooks/useRoom";
 import {useParams} from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
+import * as signalR from "@microsoft/signalr";
+import MessageView from "../../components/MessageView/MessageView";
+import {Message} from "../../types/dataTypes";
 
 import "./ChatRoomPage.css"
 
+const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .build();
 
 const ChatRoomPage: React.FC = () => {
     const { id } = useParams<{id: string}>();
@@ -14,6 +20,18 @@ const ChatRoomPage: React.FC = () => {
     useEffect(() => {
         fetchRoom(id);
     }, []);
+
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    hubConnection.on("receiveMessage", (message: Message) => {
+        setMessages(messages => {
+            let a = messages.filter(x => x.id == message.id)
+            if (a.length > 0)
+                return messages;
+            console.log(message, messages, [...messages, message])
+            return [...messages, message]
+        })
+    });
     
     if (loading){
         return (
@@ -22,9 +40,10 @@ const ChatRoomPage: React.FC = () => {
     }
     
     if (room) {
+        console.log(messages)
         return (
             <div>
-                <h1>{room.admin.userName}</h1>
+                <MessageView messages={[]} />
             </div>
         )
     }
