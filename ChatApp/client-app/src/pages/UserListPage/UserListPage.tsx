282083d9ff8d51fdc3f49dbf358b2c5e25690a47
Ilya Pivovarov <pivovarov.ilya98@gmail.com>
2022-01-02 22:24:58 +0300

    Add material ui

--- ChatApp/client-app/src/pages/UserListPage/UserListPage.tsx
+++ ChatApp/client-app/src/pages/UserListPage/UserListPage.tsx
@@ -1,13 +1,36 @@
+import {Autocomplete, TextField, Typography} from '@mui/material';
 import React from 'react';
 
-const UserPageList : React.FC = () => {
+const top100Films = [
+    {
+        title: 'Asd'
+    }
+]
+
+const UserPageList: React.FC = () => {
     return (
         <div className={"page"}>
             <div className={"page-head"}>
-                <h3>All user lists</h3>
+                <Typography>All user lists</Typography>
+                <Autocomplete
+                    id="user-search"
+                    size={"small"}
+                    disableClearable
+                    options={top100Films.map((option) => option.title)}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Search input"
+                            InputProps={{
+                                ...params.InputProps,
+                                type: 'search',
+                            }}
+                        />
+                    )}
+                />
             </div>
             <div className={"page-body"}>
-                
+
             </div>
         </div>
     );
